b94c2292dce5ad6b767c12fa368ac68fee5dc3e0
Christian <christianburberry@gmail.com>
2025-05-26 12:27:18 +0100

    AudioManager changes, asset attributions. Download the audio folder separately due to bandwidth constraints.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,11 +1,18 @@
+using AYellowpaper.SerializedCollections;
 using NaughtyAttributes;
+using RuntimeStatics;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
+
+    [Header("Audio Files")]
+    public List<SerializedKeyValuePair<AudioClip, float>> PeacefulBGMClips;
+    public SerializedKeyValuePair<AudioClip, float> BattleBGMClip;
 
     [Header("Voice")]
     public AudioSource VoiceSource;
@@ -23,7 +30,12 @@ public class AudioManager : MonoBehaviour
     [Expandable]
     private AudioSettings audioSettings;
 
+    [SerializeField]
+    private float timeBetweenBGM;
+
     private AudioSource tempSfxAudioSource;
+    private AudioClip lastPlayedBgm;
+    private float elapsedIdleTime;
 
     private void Awake()
     {
@@ -37,13 +49,70 @@ public class AudioManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+        BackgroundMusic.loop = false;
+    }
+
+    private void Start()
+    {
+        PlayRandomPeacefulBGM();
+    }
+
+    private void Update()
+    {
+        if (!CurrentlyPlayingBackgroundMusic && Time.timeScale > 0f) 
+        {
+            elapsedIdleTime += Time.deltaTime;
+            if (elapsedIdleTime > timeBetweenBGM) 
+            {
+                PlayRandomPeacefulBGM();
+                elapsedIdleTime = 0f;
+            }
+        }
+
+        if (!BackgroundMusic.loop && CurrentlyPlayingBackgroundMusic) 
+        {
+            if (BackgroundMusic.time > BackgroundMusic.clip.length * 0.95f) 
+            {
+                //Fade down slowly over 5s
+                BackgroundMusic.volume -= Time.deltaTime * 0.2f;
+            }
+        }
+    }
+
+    [Button("PlayRandomPeacefulBGM")]
+    public void PlayRandomPeacefulBGM()
+    {
+        if (lastPlayedBgm == null)
+        {
+            var random = PeacefulBGMClips.PickRandom();
+            PlayBackgroundMusic(random.Key, random.Value);
+        }
+        else 
+        {
+            var random = PeacefulBGMClips.Where(x => x.Key != lastPlayedBgm).PickRandom();
+            PlayBackgroundMusic(random.Key, random.Value);
+        }
+        BackgroundMusic.loop = false;
+    }
+
+    [Button("PlayBattleThemeLooping")]
+    public void PlayBattleThemeLooping()
+    {
+        if (CurrentlyPlayingBackgroundMusic && Application.isPlaying)
+        {
+            CrossfadeBackgroundMusic(BattleBGMClip.Key, BattleBGMClip.Value);
+        }
+        else
+        {
+            PlayBackgroundMusic(BattleBGMClip.Key, BattleBGMClip.Value);
+        }
         BackgroundMusic.loop = true;
     }
 
-    public void PlayVoiceClip(AudioClip clip)
+    public void PlayVoiceClip(AudioClip clip, float volume)
     {
         VoiceSource.clip = clip;
-        VoiceSource.volume = audioSettings.VoiceVolume;
+        VoiceSource.volume = volume * audioSettings.VoiceVolume;
         VoiceSource.Play();
     }
 
@@ -52,18 +121,21 @@ public class AudioManager : MonoBehaviour
         VoiceSource.Stop();
     }
 
-    public void PlayBackgroundMusic(AudioClip clip)
+    public void PlayBackgroundMusic(AudioClip clip, float volume = 1f)
     {
         BackgroundMusic.clip = clip;
-        BackgroundMusic.volume = audioSettings.BackgroundMusicVolume;
+        BackgroundMusic.volume = volume * audioSettings.BackgroundMusicVolume;
         BackgroundMusic.Play();
+        lastPlayedBgm = clip;
     }
 
+    [Button("StopBackgroundMusic")]
     public void StopBackgroundMusic()
     {
         BackgroundMusic.Stop();
     }
 
+    [Button("StopBackgroundMusic (Crossfade)")]
     public void StopBackgroundMusic(float fadeOutTime = 1f, bool destroy = true)
     {
         CrossfadeAudioSourceDown(BackgroundMusic, fadeOutTime, destroy);
@@ -74,7 +146,7 @@ public class AudioManager : MonoBehaviour
         BackgroundMusic.Pause();
     }
 
-    public void CrossfadeBackgroundMusic(AudioClip clip, float crossfadeTime = 1f)
+    public void CrossfadeBackgroundMusic(AudioClip clip, float volume, float crossfadeTime = 1f)
     {
         AudioSource newBGSource = Instantiate(BackgroundMusic.gameObject, transform).GetComponent<AudioSource>();
         newBGSource.gameObject.name = BackgroundMusic.gameObject.name;
@@ -89,20 +161,20 @@ public class AudioManager : MonoBehaviour
         BackgroundMusic.Play();
 
         StartCoroutine(CrossfadeAudioSourceDown(oldBGSource, crossfadeTime));
-        StartCoroutine(CrossfadeAudioSourceUp(BackgroundMusic, crossfadeTime));
+        StartCoroutine(CrossfadeAudioSourceUp(BackgroundMusic, volume * audioSettings.BackgroundMusicVolume, crossfadeTime));
     }
 
-    public void PlaySound(AudioClip clip)
+    public void PlaySound(AudioClip clip, float volume)
     {
-        StartCoroutine(PlaySoundCoroutine(clip));
+        StartCoroutine(PlaySoundCoroutine(clip, volume));
     }
 
-    public void PlaySound(AudioClip clip, float extraPitch)
+    public void PlaySound(AudioClip clip, float volume, float extraPitch)
     {
-        StartCoroutine(PlaySoundCoroutine(clip, extraPitch));
+        StartCoroutine(PlaySoundCoroutine(clip, volume, extraPitch));
     }
 
-    public IEnumerator PlaySoundCoroutine(AudioClip clip, float extraPitch = 0f)
+    public IEnumerator PlaySoundCoroutine(AudioClip clip, float volume, float extraPitch = 0f)
     {
         GameObject tempSoundPlayer = new GameObject(clip.name);
         tempSoundPlayer.transform.SetParent(SfxParent);
@@ -111,7 +183,7 @@ public class AudioManager : MonoBehaviour
         tempSfxAudioSource = source;
         source.clip = clip;
         source.pitch += extraPitch;
-        source.volume = audioSettings.SfxVolume;
+        source.volume = volume * audioSettings.SfxVolume;
         source.Play();
 
         while (source.isPlaying) yield return null;
@@ -119,9 +191,9 @@ public class AudioManager : MonoBehaviour
         Destroy(tempSoundPlayer);
     }
 
-    private IEnumerator CrossfadeAudioSourceUp(AudioSource source, float crossfadeTime = 1f)
+    private IEnumerator CrossfadeAudioSourceUp(AudioSource source, float volume, float crossfadeTime = 1f)
     {
-        while (source.volume != 1f) { source.volume += Time.deltaTime / crossfadeTime; yield return null; }
+        while (source.volume != volume) { source.volume += Time.deltaTime / crossfadeTime; yield return null; }
     }
 
     private IEnumerator CrossfadeAudioSourceDown(AudioSource source, float crossfadeTime = 1f, bool deleteOnDone = true)
@@ -141,17 +213,27 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    [Button("PauseAllAudio")]
     private void PauseAllAudio()
     {
         BackgroundMusic.Pause();
         VoiceSource.Pause();
-        tempSfxAudioSource?.Pause();
+
+        if (tempSfxAudioSource != null) 
+        {
+            tempSfxAudioSource.Pause();
+        }
     }
 
+    [Button("UnpauseAllAudio")]
     private void UnpauseAllAudio()
     {
         BackgroundMusic.UnPause();
         VoiceSource.UnPause();
-        tempSfxAudioSource?.UnPause();
+
+        if (tempSfxAudioSource != null)
+        {
+            tempSfxAudioSource.UnPause();
+        }
     }
 }
